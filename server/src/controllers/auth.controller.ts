5bcd7de267ab28c02f11c0ffd837a78f08b009c1
Grover Soans <groverpsoans@gmail.com>
2024-11-16 15:46:06 -0600

    refresh cookies endpoint implemented

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 import catchErrors from "../utils/catchErrors";
-import { createAccount, loginUser } from "../service/auth.service";
-import { CREATED, OK } from "../constants/http";
-import { setAuthCookies } from "../utils/cookies";
+import { createAccount, loginUser, refreshUserAccessToken } from "../service/auth.service";
+import { CREATED, OK, UNAUTHORIZED } from "../constants/http";
+import { clearAuthCookies, getAccessTokenCookieOptions, getRefreshTokenCookieOptions, setAuthCookies } from "../utils/cookies";
 import { loginSchema, registerSchema } from "./auth.schemas";
-
-
-
+import { verifyToken } from "../utils/jwt";
+import SessionModel from "../models/session.model";
+import appAssert from "../utils/appAssert";
 
 export const registerHandler = catchErrors(async(req, res) => {
     //validate request
@@ -42,3 +42,39 @@ export const loginHandler = catchErrors(async (req, res) => {
         message: "Login Successful"
     });
 });
+
+export const logoutHandler = catchErrors(async (req, res) =>{
+    const accessToken = req.cookies.accessToken as string|undefined;;
+    const {payload} = verifyToken(accessToken || "")
+
+    if (payload) {
+        await SessionModel.findByIdAndDelete(payload.sessionId);
+    }
+
+    return clearAuthCookies(res).
+    status(OK).json({
+        message: "Logout successful",
+    })
+});
+
+export const refreshHandler = catchErrors(async (req, res) => {
+    const refreshToken = req.cookies.refreshToken as string|undefined;
+    appAssert(refreshToken, UNAUTHORIZED, "Missing refresh Token");
+
+    const {
+        accessToken,
+        newRefreshToken
+    } = await refreshUserAccessToken(refreshToken);
+
+    if(newRefreshToken) {
+        res.cookie("refreshToken", newRefreshToken, getRefreshTokenCookieOptions());
+    }
+
+    return res
+    .status(OK)
+    .cookie("accessToken", accessToken, getAccessTokenCookieOptions())
+    .json({
+        message: "Access token refreshed",
+    })
+});
+
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginHandler, registerHandler } from "../controllers/auth.controller";
+import { loginHandler, logoutHandler, refreshHandler, registerHandler } from "../controllers/auth.controller";
 
 
 const authRoutes = Router();
@@ -8,5 +8,7 @@ const authRoutes = Router();
 
 authRoutes.post("/register", registerHandler);
 authRoutes.post("/login", loginHandler);
+authRoutes.get("/logout", logoutHandler);
+authRoutes.get("/refresh", refreshHandler);
 
 export default authRoutes;
