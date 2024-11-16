060087b032f1b3f5158de4c1bc91aa52e7965d48
Grover Soans <groverpsoans@gmail.com>
2024-11-16 16:49:06 -0600

    reset password endpoint implemented

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginHandler, logoutHandler, refreshHandler, registerHandler, verifyEmailHandler } from "../controllers/auth.controller";
+import { loginHandler, logoutHandler, refreshHandler, registerHandler, resetPasswordHandler, sendPasswordResetHandler, verifyEmailHandler } from "../controllers/auth.controller";
 
 
 const authRoutes = Router();
@@ -8,6 +8,8 @@ const authRoutes = Router();
 
 authRoutes.post("/register", registerHandler);
 authRoutes.post("/login", loginHandler);
+authRoutes.post("/password/forgot", sendPasswordResetHandler);
+authRoutes.post("/password/reset", resetPasswordHandler);
 
 authRoutes.get("/logout", logoutHandler);
 authRoutes.get("/refresh", refreshHandler);
